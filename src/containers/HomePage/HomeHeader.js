a2d714872a81e6eb4ccb9c5490925282e9cd67ac
Phuong Nam <dophnamm@gmail.com>
2021-10-07 22:12:12 +0700

    css header choose language vi en

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './HomeHeader.scss';
+import { FormattedMessage } from 'react-intl';
 
 class HomeHeader extends Component {
     render() {
+        console.log('check props' , this.props)
         return (
+            <>
                 <div className="home-header-container">
                     <div className="home-header-content">
                         <div className="left-content">
@@ -19,58 +22,118 @@ class HomeHeader extends Component {
                         <div className="center-content">
                             <div className="child-content">
                                 <div>
-                                    <b> Chuyên Khoa </b>
+                                    <b> <FormattedMessage id="homeHeader.specialty"/> </b>
                                 </div>
                                 <div>
-                                    <p> Tìm bác sĩ theo chuyên khoa </p>
-                                </div>
-                            </div>
-
-                            <div className="child-content">
-                                <div>
-                                    <b> Cơ Sở Y Tế </b>
-                                </div>
-                                <div>
-                                    <p> Chọn bệnh viện phòng khám </p>
+                                    <p> <FormattedMessage id="homeHeader.searchdoctor"/> </p>
                                 </div>
                             </div>
 
                             <div className="child-content">
                                 <div>
-                                    <b> Bác Sĩ </b>
+                                    <b> <FormattedMessage id="homeHeader.health"/> </b>
                                 </div>
                                 <div>
-                                    <p> Chọn bác sĩ giỏi </p>
+                                    <p> <FormattedMessage id="homeHeader.selectroom"/> </p>
                                 </div>
                             </div>
 
                             <div className="child-content">
                                 <div>
-                                    <b> Gói Khám </b>
+                                    <b> <FormattedMessage id="homeHeader.doctor"/> </b>
                                 </div>
                                 <div>
-                                    <p> Khám sức khoẻ tổng quát </p>
+                                    <p> <FormattedMessage id="homeHeader.selectdoctor"/> </p>
+                                </div>
+                            </div>
+
+                            <div className="child-content">
+                                <div>
+                                    <b> <FormattedMessage id="homeHeader.fee"/> </b>
+                                </div>
+                                <div>
+                                    <p> <FormattedMessage id="homeHeader.checkhealth"/> </p>
                                 </div>
                             </div>
                         </div>
 
                         <div className="right-content">
                             <div className="support">
-                                <i class="far fa-question-circle"></i> Hỗ Trợ
+                                <i className="far fa-question-circle"></i> 
+                                <FormattedMessage id="homeHeader.support"/>
                             </div>
-                            <div className="flag">
-                                VN
+                            <div className="choose-language">
+                                <div className="language-vi">VI</div>
+                                <div className="language-en">EN</div>
                             </div>
                         </div>
                     </div>
                 </div>
+                
+                <div className="home-header-banner">
+                    <div className="banner-header-content">
+                        <h2><FormattedMessage id="banner.h2-1"/></h2>
+                        <h2><FormattedMessage id="banner.h2-2"/></h2>
+                    </div>
+
+                    <div className="search">
+                        <i className="fas fa-search"></i>
+                        <input type="text" placeholder="Tìm kiếm..."/>
+                    </div>
+
+                    <div className="option">
+                        <div className="option-item">
+                            <div className="item-icon">
+                                <img src="https://cdn1.iconfinder.com/data/icons/medical-2-19/512/medical-healthcare-hospital-02-512.png" alt="item1"/>
+                            </div>
+                            <p className="item-text"><FormattedMessage id="option.itemone"/></p>
+                        </div>
+
+                        <div className="option-item">
+                            <div className="item-icon">
+                                <img src="https://cdn1.iconfinder.com/data/icons/medical-2-19/512/medical-healthcare-hospital-32-512.png" alt="item1"/>
+                            </div>
+                            <p className="item-text"><FormattedMessage id="option.itemtwo"/></p>
+                        </div>
+
+                        <div className="option-item">
+                            <div className="item-icon">
+                                <img src="https://cdn1.iconfinder.com/data/icons/medical-2-19/512/medical-healthcare-hospital-33-512.png" alt="item1"/>
+                            </div>
+                            <p className="item-text"><FormattedMessage id="option.itemthree"/></p>
+                        </div>
+
+                        <div className="option-item">
+                            <div className="item-icon">
+                                <img src="https://cdn1.iconfinder.com/data/icons/medical-2-19/512/medical-healthcare-hospital-01-512.png" alt="item1"/>
+                            </div>
+                            <p className="item-text"><FormattedMessage id="option.itemfour"/></p>
+                        </div>
+
+                        <div className="option-item">
+                            <div className="item-icon">
+                                <img src="https://cdn1.iconfinder.com/data/icons/medical-2-19/512/medical-healthcare-hospital-34-512.png" alt="item1"/>
+                            </div>
+                            <p className="item-text"><FormattedMessage id="option.itemfive"/></p>
+                        </div>
+
+                        <div className="option-item">
+                            <div className="item-icon">
+                                <img src="https://cdn1.iconfinder.com/data/icons/medical-2-19/512/medical-healthcare-hospital-27-512.png" alt="item1"/>
+                            </div>
+                            <p className="item-text"><FormattedMessage id="option.itemsix"/></p>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language
     };
 };
 
