ef92183ba0bf13ba88b8fd0bfbbc0025adea213b
Phuong Nam <dophnamm@gmail.com>
2021-11-14 13:57:34 +0700

    fix warning buffer

--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -1,100 +1,102 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './OutStandingDoctor.scss';
-import { FormattedMessage } from 'react-intl';
-import Slider from 'react-slick';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./OutStandingDoctor.scss";
+import { FormattedMessage } from "react-intl";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import SpecialtyImg from "../../../assets/specialty/co-xuong-khop.png";
 import * as actions from "../../../store/actions";
 import { LANGUAGES } from "../../../utils/";
-import { withRouter } from 'react-router';
-
+import { withRouter } from "react-router";
 
 class OutStandingDoctor extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            arrDoctors: []
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			arrDoctors: [],
+		};
+	}
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevProps.topDoctorsRedux !== this.props.topDoctorsRedux) {
-            this.setState({
-                arrDoctors: this.props.topDoctorsRedux
-            })
-        }
-    }
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		if (prevProps.topDoctorsRedux !== this.props.topDoctorsRedux) {
+			this.setState({
+				arrDoctors: this.props.topDoctorsRedux,
+			});
+		}
+	}
 
-    componentDidMount() {
-        this.props.loadTopDoctors()
-    }
+	componentDidMount() {
+		this.props.loadTopDoctors();
+	}
 
-    handleViewDetailDoctor = (doctor) => {
-        if (this.props.history) {
-            this.props.history.push(`/detail-doctor/${doctor.id}`)
-        }
-    }
+	handleViewDetailDoctor = (doctor) => {
+		if (this.props.history) {
+			this.props.history.push(`/detail-doctor/${doctor.id}`);
+		}
+	};
 
-    render() {
-        let arrDoctors = this.state.arrDoctors;
-        let { language } = this.props
-        return (
-            <div className="section-outstanding-doctor">
-                <div className="section-content">
+	render() {
+		let arrDoctors = this.state.arrDoctors;
+		let { language } = this.props;
+		return (
+			<div className="section-outstanding-doctor">
+				<div className="section-content">
+					<div className="section-header">
+						<div className="sub-header">
+							<FormattedMessage id="homepage.outstanding-doctor" />
+						</div>
+						<button>
+							<FormattedMessage id="homepage.more" />
+						</button>
+					</div>
 
-                    <div className="section-header">
-                        <div className="sub-header">
-                            <FormattedMessage id="homepage.outstanding-doctor" />
-                        </div>
-                        <button>
-                            <FormattedMessage id="homepage.more" />
-                        </button>
-                    </div>
-
-                    <Slider {...this.props.settings}>
-                        {
-                            arrDoctors && arrDoctors.length > 0 &&
-                            arrDoctors.map((item, index) => {
-                                let imageBase64 = ''
-                                if (item.image) {
-                                    imageBase64 = new Buffer(item.image, 'base64').toString('binary');
-                                }
-                                let nameVi = `${item.positionData.valueVi}, ${item.firstName} ${item.lastName}`
-                                let nameEn = `${item.positionData.valueEn}, ${item.firstName} ${item.lastName}`
-                                return (
-                                    <div className="item-block" key={index}
-                                        onClick={() => this.handleViewDetailDoctor(item)}
-                                    >
-                                        <div className="item">
-                                            <img src={imageBase64} alt={item.firstName} />
-                                            <p>{language === LANGUAGES.VI ? nameVi : nameEn}</p>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </Slider>
-                </div>
-            </div>
-        );
-    }
+					<Slider {...this.props.settings}>
+						{arrDoctors &&
+							arrDoctors.length > 0 &&
+							arrDoctors.map((item, index) => {
+								let imageBase64 = "";
+								if (item.image) {
+									imageBase64 = Buffer.from(item.image, "base64").toString("binary");
+								}
+								let nameVi = `${item.positionData.valueVi}, ${item.firstName} ${item.lastName}`;
+								let nameEn = `${item.positionData.valueEn}, ${item.firstName} ${item.lastName}`;
+								return (
+									<div
+										className="item-block"
+										key={index}
+										onClick={() => this.handleViewDetailDoctor(item)}
+									>
+										<div className="item">
+											<img src={imageBase64} alt={item.firstName} />
+											<p>
+												{language === LANGUAGES.VI
+													? nameVi
+													: nameEn}
+											</p>
+										</div>
+									</div>
+								);
+							})}
+					</Slider>
+				</div>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language,
-        topDoctorsRedux: state.admin.topDoctor
-    };
+const mapStateToProps = (state) => {
+	return {
+		isLoggedIn: state.user.isLoggedIn,
+		language: state.app.language,
+		topDoctorsRedux: state.admin.topDoctor,
+	};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        loadTopDoctors: () => dispatch(actions.fetchTopDoctor())
-
-    };
+const mapDispatchToProps = (dispatch) => {
+	return {
+		loadTopDoctors: () => dispatch(actions.fetchTopDoctor()),
+	};
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor));
